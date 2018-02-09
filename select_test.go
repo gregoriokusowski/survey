817bd229502c49dbe85b1dac9dff355d3a408654
Gregório Kusowski <gregorio.kusowski@gmail.com>
2018-02-09 17:39:31 +0100

    Unmess select_test

--- select_test.go
+++ select_test.go
@@ -1,212 +1,89 @@
 package survey
 
 import (
-	"errors"
-	"fmt"
-	"os"
+	"bytes"
+	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"gopkg.in/AlecAivazis/survey.v1/core"
 	"gopkg.in/AlecAivazis/survey.v1/terminal"
 )
 
-/*
-Select is a prompt that presents a list of various options to the user
-for them to select using the arrow keys and enter. Response type is a string.
-
-	color := ""
-	prompt := &survey.Select{
-		Message: "Choose a color:",
-		Options: []string{"red", "blue", "green"},
-	}
-	survey.AskOne(prompt, &color, nil)
-*/
-type Select struct {
-	core.Renderer
-	Message         string
-	Options         []string
-	Default         string
-	StringerOptions []fmt.Stringer
-	StringerDefault fmt.Stringer
-	Help            string
-	PageSize        int
-	selectedIndex   int
-	useDefault      bool
-	showingHelp     bool
+func init() {
+	// disable color output for all prompts to simplify testing
+	core.DisableColor = true
 }
 
-// the data available to the templates when processing
-type SelectTemplateData struct {
-	Select
-	PageEntries   []string
-	SelectedIndex int
-	Answer        string
-	ShowAnswer    bool
-	ShowHelp      bool
-}
+func TestSelectRender(t *testing.T) {
 
-var SelectQuestionTemplate = `
-{{- if .ShowHelp }}{{- color "cyan"}}{{ HelpIcon }} {{ .Help }}{{color "reset"}}{{"\n"}}{{end}}
-{{- color "green+hb"}}{{ QuestionIcon }} {{color "reset"}}
-{{- color "default+hb"}}{{ .Message }}{{color "reset"}}
-{{- if .ShowAnswer}}{{color "cyan"}} {{.Answer}}{{color "reset"}}{{"\n"}}
-{{- else}}
-  {{- if and .Help (not .ShowHelp)}} {{color "cyan"}}[{{ HelpInputRune }} for help]{{color "reset"}}{{end}}
-  {{- "\n"}}
-  {{- range $ix, $choice := .PageEntries}}
-    {{- if eq $ix $.SelectedIndex}}{{color "cyan+b"}}{{ SelectFocusIcon }} {{else}}{{color "default+hb"}}  {{end}}
-    {{- $choice}}
-    {{- color "reset"}}{{"\n"}}
-  {{- end}}
-{{- end}}`
-
-// OnChange is called on every keypress.
-func (s *Select) OnChange(line []rune, pos int, key rune) (newLine []rune, newPos int, ok bool) {
-	// if the user pressed the enter key
-	if key == terminal.KeyEnter {
-		return []rune(s.StringerOptions[s.selectedIndex].String()), 0, true
-		// if the user pressed the up arrow
-	} else if key == terminal.KeyArrowUp {
-		s.useDefault = false
-
-		// if we are at the top of the list
-		if s.selectedIndex == 0 {
-			// start from the button
-			s.selectedIndex = len(s.StringerOptions) - 1
-		} else {
-			// otherwise we are not at the top of the list so decrement the selected index
-			s.selectedIndex--
-		}
-		// if the user pressed down and there is room to move
-	} else if key == terminal.KeyArrowDown {
-		s.useDefault = false
-		// if we are at the bottom of the list
-		if s.selectedIndex == len(s.StringerOptions)-1 {
-			// start from the top
-			s.selectedIndex = 0
-		} else {
-			// increment the selected index
-			s.selectedIndex++
-		}
-		// only show the help message if we have one
-	} else if key == core.HelpInputRune && s.Help != "" {
-		s.showingHelp = true
+	prompt := Select{
+		Message: "Pick your word:",
+		Options: []string{"foo", "bar", "baz", "buz"},
+		Default: "baz",
 	}
 
-	// figure out the options and index to render
-	opts, idx := paginate(s.PageSize, s.StringerOptions, s.selectedIndex)
+	helpfulPrompt := prompt
+	helpfulPrompt.Help = "This is helpful"
 
-	// render the options
-	s.Render(
-		SelectQuestionTemplate,
-		SelectTemplateData{
-			Select:        *s,
-			SelectedIndex: idx,
-			ShowHelp:      s.showingHelp,
-			PageEntries:   opts,
+	tests := []struct {
+		title    string
+		prompt   Select
+		data     SelectTemplateData
+		expected string
+	}{
+		{
+			"Test Select question output",
+			prompt,
+			SelectTemplateData{SelectedIndex: 2, PageEntries: prompt.Options},
+			`? Pick your word:
+  foo
+  bar
+❯ baz
+  buz
+`,
 		},
-	)
-
-	// if we are not pressing ent
-	return []rune(s.StringerOptions[s.selectedIndex].String()), 0, true
-}
-
-func (s *Select) Prompt() (interface{}, error) {
-	// if there are no options to render
-	if len(s.StringerOptions) == 0 {
-		// we failed
-		return "", errors.New("please provide options to select from")
-	}
-
-	// start off with the first option selected
-	sel := 0
-	// if there is a default
-	if s.StringerDefault != nil {
-		// find the choice
-		for i, opt := range s.StringerOptions {
-			// if the option correponds to the default
-			if opt.String() == s.StringerDefault.String() {
-				// we found our initial value
-				sel = i
-				// stop looking
-				break
-			}
-		}
-	}
-	// save the selected index
-	s.selectedIndex = sel
-
-	// figure out the options and index to render
-	opts, idx := paginate(s.PageSize, s.StringerOptions, sel)
-
-	// ask the question
-	err := s.Render(
-		SelectQuestionTemplate,
-		SelectTemplateData{
-			Select:        *s,
-			PageEntries:   opts,
-			SelectedIndex: idx,
+		{
+			"Test Select answer output",
+			prompt,
+			SelectTemplateData{Answer: "buz", ShowAnswer: true, PageEntries: prompt.Options},
+			"? Pick your word: buz\n",
 		},
-	)
-	if err != nil {
-		return "", err
-	}
-
-	// hide the cursor
-	terminal.CursorHide()
-	// show the cursor when we're done
-	defer terminal.CursorShow()
-
-	// by default, use the default value
-	s.useDefault = true
-
-	rr := terminal.NewRuneReader(os.Stdin)
-	rr.SetTermMode()
-	defer rr.RestoreTermMode()
-	// start waiting for input
-	for {
-		r, _, err := rr.ReadRune()
-		if err != nil {
-			return "", err
-		}
-		if r == '\r' || r == '\n' {
-			break
-		}
-		if r == terminal.KeyInterrupt {
-			return "", terminal.InterruptErr
-		}
-		if r == terminal.KeyEndTransmission {
-			break
-		}
-		s.OnChange(nil, 0, r)
-	}
-
-	var val fmt.Stringer
-	// if we are supposed to use the default value
-	if s.useDefault {
-		// if there is a default value
-		if s.StringerDefault != nil {
-			// use the default value
-			val = s.StringerDefault
-		} else {
-			// there is no default value so use the first
-			val = s.StringerOptions[0]
-		}
-		// otherwise the selected index points to the value
-	} else {
-		// the
-		val = s.StringerOptions[s.selectedIndex]
-	}
-
-	return val, err
-}
-
-func (s *Select) Cleanup(val interface{}) error {
-	return s.Render(
-		SelectQuestionTemplate,
-		SelectTemplateData{
-			Select:     *s,
-			Answer:     val.(string),
-			ShowAnswer: true,
+		{
+			"Test Select question output with help hidden",
+			helpfulPrompt,
+			SelectTemplateData{SelectedIndex: 2, PageEntries: prompt.Options},
+			`? Pick your word: [? for help]
+  foo
+  bar
+❯ baz
+  buz
+`,
 		},
-	)
+		{
+			"Test Select question output with help shown",
+			helpfulPrompt,
+			SelectTemplateData{SelectedIndex: 2, ShowHelp: true, PageEntries: prompt.Options},
+			`ⓘ This is helpful
+? Pick your word:
+  foo
+  bar
+❯ baz
+  buz
+`,
+		},
+	}
+
+	outputBuffer := bytes.NewBufferString("")
+	terminal.Stdout = outputBuffer
+
+	for _, test := range tests {
+		outputBuffer.Reset()
+		test.data.Select = test.prompt
+		err := test.prompt.Render(
+			SelectQuestionTemplate,
+			test.data,
+		)
+		assert.Nil(t, err, test.title)
+		assert.Equal(t, test.expected, outputBuffer.String(), test.title)
+	}
 }
